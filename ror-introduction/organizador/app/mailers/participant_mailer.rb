d892704f64567dc27928ed3728253ff424c59be5
Max Zeballos <max.zeballos@rankmi.com>
2020-12-08 20:45:06 -0500

    remove organizador

--- ror-introduction/organizador/app/mailers/participant_mailer.rb
+++
@@ -1,7 +0,0 @@
-class ParticipantMailer < ApplicationMailer
-    def new_task_email
-        @user = params[:user]
-        @task = params[:task]
-        mail to: @user.email, subject: 'Tarea Asignada'
-    end
-end
--- ror-introduction/organizador/db/migrate/20200920122328_add_code_to_task.rb
+++
@@ -1,5 +0,0 @@
-class AddCodeToTask < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tasks, :code, :string
-  end
-end
