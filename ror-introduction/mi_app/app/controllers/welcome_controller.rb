4bf6e4ae91548274fc30c569c86154276ebda4ba
Max Zeballos <max.zeballos@rankmi.com>
2020-12-08 20:32:53 -0500

    delete mi_app proyect

--- ror-introduction/mi_app/app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-    def hello
-        @credentials = Rails.application.credentials.hello
-        @variable = Pet.first.name
-    end
-end
--- ror-introduction/mi_app/app/views/books/_book.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! book, :id, :title, :author, :description, :isbn, :pages_count, :created_at, :updated_at
-json.url book_url(book, format: :json)
